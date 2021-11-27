3b0468e08920795db6db96a66a12d879e0a13648
felipendc <felipendc10@gmail.com>
2021-11-27 11:44:17 -0300

    Vicyos Rolling Update: (sáb 27 nov 2021 11:44:17 -03)

---
+++ upall/2_send_files_to_github.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# github.com/felipendc
+
+# Go back to the root (main) folder
+cd ../
+
+# # It will print the output of the command "date"
+output=$(date)
+
+
+# Below command will backup everything inside the project folder
+git add . 
+
+# # This will print the date and the hour that this project was uploaded to github
+git commit -m "Vicyos Rolling Update: ($output)"
+
+#Send the local files to github
+git push 
+
+echo "################################################################"
+echo "###################    Git Push Done      ######################"
+echo "################################################################"
+
